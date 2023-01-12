67d033d903dbc9be838d5999073bfab88c003a3a
Obasi Chiazoka <97561206+MichaelSparks004@users.noreply.github.com>
2023-01-12 01:39:48 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Install a certain version of flask (2.1.0)
+# Install a certain version of flask (2.2.2)
 
 package { 'flask':
-  ensure => '2.1.0',
+  ensure => '2.2.2',
   provider => 'pip3',
 }
