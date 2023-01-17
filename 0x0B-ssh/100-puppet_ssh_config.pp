c5a9f543d469b2fdbdc69e966be48c30d83a1e4e
Obasi Chiazoka <97561206+MichaelSparks004@users.noreply.github.com>
2023-01-17 01:21:08 +0100

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -10,16 +10,4 @@ exec { 'Declare identity file':
 exec { 'Turn on pubkey auth':
   command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
   path    => '/usr/bin:/usr/sbin:/bin'
-=======
-# Puppet script to create ssh config file
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-}
-
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
 }
